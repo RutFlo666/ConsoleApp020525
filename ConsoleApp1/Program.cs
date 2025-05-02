2531cf896c5d658c891814d4fdfbdbf2499b6f1d
RutFlo666 <154447740+RutFlo666@users.noreply.github.com>
2025-05-02 15:16:00 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            MaxLINQ();
+
+            //Shows The oldest person in the list.
+        }
+
+        static void MaxLINQ()
+        {
+            var oldestPerson = PeopleList.people
+                .Max(x => x.Age);
+
+            Console.WriteLine("Kõige vanem inimene on " + oldestPerson + " aastane.");
+        }
+    
+    }
+}
